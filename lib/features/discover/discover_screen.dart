8e052f0ed357c330c6fbccc2a9718a079d9f4600
jinwook song <wlsdnr129@naver.com>
2023-08-07 22:40:45 +0900

    widget: FadeInImage & DefaultTextStyle

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_v2/constants/gaps.dart';
 import 'package:tiktok_v2/constants/sizes.dart';
 
 const tabs = [
@@ -48,12 +50,65 @@ class DiscoverScreen extends StatelessWidget {
                 crossAxisCount: 2,
                 crossAxisSpacing: Sizes.size10,
                 mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 16,
+                childAspectRatio: 9 / (16 + 4),
               ),
-              itemBuilder: (context, index) => Container(
-                color: Colors.amber,
-                child: Center(
-                  child: Text('$index'),
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: Sizes.size6),
+                child: Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 9 / 16,
+                      child: FadeInImage.assetNetwork(
+                        fit: BoxFit.cover,
+                        placeholder: 'assets/images/placeholder.jpeg',
+                        image: "https://source.unsplash.com/random/?$index",
+                      ),
+                    ),
+                    Gaps.v5,
+                    const Text(
+                      'Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Gaps.v5,
+                    DefaultTextStyle(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: Sizes.size12,
+                        color: Colors.grey.shade500,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const CircleAvatar(
+                            radius: Sizes.size12,
+                            backgroundImage: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/78011042?v=4'),
+                          ),
+                          Gaps.h6,
+                          const Expanded(
+                            child: Text(
+                              'Avatar description is very long',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Gaps.h2,
+                          Icon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size12,
+                            color: Colors.grey.shade500,
+                          ),
+                          Gaps.h2,
+                          const Text('2.0M')
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
