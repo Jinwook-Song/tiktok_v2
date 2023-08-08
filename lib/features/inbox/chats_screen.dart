aa300b6e7011347da41ff93a0db420c11c9bc986
jinwook song <wlsdnr129@naver.com>
2023-08-08 23:52:55 +0900

    widget: delete animate with AnimatedList2

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -12,17 +12,70 @@ class ChatsScreen extends StatefulWidget {
 class _ChatsScreenState extends State<ChatsScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
 
+  final Duration _duration = const Duration(milliseconds: 300);
   final List<int> _items = [];
+
   void _addItem() {
     if (_key.currentState != null) {
-      _key.currentState!.insertItem(
-        _items.length,
-        duration: const Duration(
-          milliseconds: 300,
-        ),
-      );
+      _key.currentState!.insertItem(_items.length, duration: _duration);
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int item) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        item,
+        (context, animation) {
+          return SizeTransition(
+            sizeFactor: animation,
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              child: _makeListTile(item: item, deleted: true),
+            ),
+          );
+        },
+        duration: _duration,
+      );
+
+      _items.removeAt(item);
+    }
+  }
+
+  Widget _makeListTile({required int item, bool deleted = false}) {
+    return ListTile(
+      onLongPress: () => _deleteItem(item),
+      leading: const CircleAvatar(
+        radius: Sizes.size32,
+        foregroundImage: NetworkImage(
+          "https://avatars.githubusercontent.com/u/3612017",
+        ),
+        child: Text('Nico'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "Nico ($item)",
+            style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: deleted ? Colors.white : Colors.black),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: Text(
+        "Don't forget to make video",
+        style: TextStyle(color: deleted ? Colors.white : Colors.black),
+      ),
+    );
   }
 
   @override
@@ -51,33 +104,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
             opacity: animation,
             child: SizeTransition(
               sizeFactor: animation,
-              child: ListTile(
-                leading: const CircleAvatar(
-                  radius: Sizes.size32,
-                  foregroundImage: NetworkImage(
-                    "https://avatars.githubusercontent.com/u/3612017",
-                  ),
-                  child: Text('Nico'),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      "Nico ($index)",
-                      style: const TextStyle(fontWeight: FontWeight.w600),
-                    ),
-                    Text(
-                      "2:16 PM",
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: Sizes.size12,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: const Text("Don't forget to make video"),
-              ),
+              child: _makeListTile(item: index),
             ),
           );
         },
