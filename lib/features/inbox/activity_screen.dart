3b850ef7c7541c37afb7d7e52a5ad356c1682f93
jinwook song <wlsdnr129@naver.com>
2023-08-08 18:44:47 +0900

    package: flutter_slidable

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_v2/constants/sizes.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ActivityScreen extends StatefulWidget {
   const ActivityScreen({super.key});
@@ -10,6 +11,7 @@ class ActivityScreen extends StatefulWidget {
 }
 
 class _ActivityScreenState extends State<ActivityScreen> {
+  bool _dismissed = false;
   final List<String> _notifications = List.generate(
     20,
     (index) => '${index}h',
@@ -41,6 +43,69 @@ class _ActivityScreenState extends State<ActivityScreen> {
                   fontWeight: FontWeight.w600),
             ),
           ),
+          if (!_dismissed)
+            Slidable(
+              // Specify a key if the Slidable is dismissible.
+              key: const ValueKey(0),
+
+              // The start action pane is the one at the left or the top side.
+              startActionPane: ActionPane(
+                // A motion is a widget used to control how the pane animates.
+                motion: const ScrollMotion(),
+
+                // A pane can dismiss the Slidable.
+                dismissible: DismissiblePane(onDismissed: () {
+                  _dismissed = true;
+                  setState(() {});
+                }),
+
+                // All actions are defined in the children parameter.
+                children: [
+                  // A SlidableAction can have an icon and/or a label.
+                  SlidableAction(
+                    onPressed: (context) => {},
+                    backgroundColor: const Color(0xFFFE4A49),
+                    foregroundColor: Colors.white,
+                    icon: Icons.delete,
+                    label: 'Delete',
+                  ),
+                  SlidableAction(
+                    onPressed: (context) => {},
+                    backgroundColor: const Color(0xFF21B7CA),
+                    foregroundColor: Colors.white,
+                    icon: Icons.share,
+                    label: 'Share',
+                  ),
+                ],
+              ),
+
+              // The end action pane is the one at the right or the bottom side.
+              endActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                children: [
+                  SlidableAction(
+                    // An action can be bigger than the others.
+                    flex: 2,
+                    onPressed: (context) => {},
+                    backgroundColor: const Color(0xFF7BC043),
+                    foregroundColor: Colors.white,
+                    icon: Icons.archive,
+                    label: 'Archive',
+                  ),
+                  SlidableAction(
+                    onPressed: (context) => {},
+                    backgroundColor: const Color(0xFF0392CF),
+                    foregroundColor: Colors.white,
+                    icon: Icons.save,
+                    label: 'Save',
+                  ),
+                ],
+              ),
+
+              // The child of the Slidable is what the user sees when the
+              // component is not dragged.
+              child: const ListTile(title: Text('Slide me')),
+            ),
           for (var notification in _notifications)
             Dismissible(
               key: Key(notification),
